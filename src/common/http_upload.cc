13b85bce946924c02ea20e32ecfffcd0eaa4b27f
Jon Turney <jon.turney@dronecode.org.uk>
2016-03-05 12:56:22 +0000

    Build minidump_upload, sym_upload for all targets
    Rather than duplicating these tools for any target which has libcurl
    
    - Introduce src/tools/common/ and move minidump_upload, sym_upload there
    from src/tools/linux/
    - Move linux/http_upload.cc,.h up to src/common/ from src/common/linux/

--- src/common/http_upload.cc
+++ src/common/http_upload.cc
@@ -27,7 +27,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "common/linux/http_upload.h"
+#include "common/http_upload.h"
 
 #include <assert.h>
 #include <curl/curl.h>
